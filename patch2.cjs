27558ba11648050a5e601fa95f6d3c9bf5a55e4c
MauroCasarin <70527971+MauroCasarin@users.noreply.github.com>
2026-04-30 12:06:49 -0300

    26

--- patch2.cjs
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-let c = fs.readFileSync('src/pages/ClientApp.tsx', 'utf8');
-
-c = c.replace(/show_cash: data\.theme\?\.show_cash !== false,\n\s*show_card: data\.theme\?\.show_card !== false,\n\s*show_transfer: data\.theme\?\.show_transfer !== false/g, "online_payments_hidden: data.theme?.online_payments_hidden || false");
-
-fs.writeFileSync('src/pages/ClientApp.tsx', c);
